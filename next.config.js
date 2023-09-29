d295d7ef8bab9a0eedbeb3552d739f8d9d16f748
Rishabh <thisisrishabhkumar@gmail.com>
2023-09-29 15:14:33 +0530

    Final Commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
     images: {
         domains: ["res.cloudinary.com", "lh3.googleusercontent.com"],
     },
+    output: "export",
+    reactStrictMode: true,
 };
 
 module.exports = nextConfig;
