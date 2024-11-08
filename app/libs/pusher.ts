5c0ce71a8d85012d191d47a8b1fa19a9d226f039
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-11-08 10:47:12 +0100

    Update pusher.ts

--- app/libs/pusher.ts
+++ app/libs/pusher.ts
@@ -1,20 +1,20 @@
 'use client';
 import PusherServer from "pusher";
 import PusherClient from "pusher-js";
-/*app_id = "1892829"
-key = "7ce91fec47e645948c24"
-secret = "97979b7f52e4f045d353"
-cluster = "mt1"*/
+let app_id = "1892829"
+let key = "7ce91fec47e645948c24"
+let secret = "97979b7f52e4f045d353"
+let cluster = "mt1"
 export const pusherServer = new PusherServer({
-    appId: process.env.PUSHER_APP_ID!,
-    key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
-    secret: process.env.PUSHER_SECRET!,
+    appId: app_id,
+    key: key,
+    secret: secret,
     cluster: "mt1"
     useTLS: true,
 });
 
 export const pusherClient = new PusherClient(
-    process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+    key,
     {
         channelAuthorization: {
             endpoint: "/api/pusher/auth",
