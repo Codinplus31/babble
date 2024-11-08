ed14a6b2f002c73e3bd4b25ecece5a62b3a152cc
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-11-08 20:43:47 +0100

    Update pusher.ts

--- app/libs/pusher.ts
+++ app/libs/pusher.ts
@@ -7,7 +7,7 @@ let secret = "97979b7f52e4f045d353"
 let cluster = "mt1"
 export const pusherServer = new PusherServer({
     appId: process.env.PUSHER_APP_ID,
-    key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY
+    key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY,
     secret: process.env.PUSHER_SECRET,
     cluster: "mt1",
     useTLS: true,
