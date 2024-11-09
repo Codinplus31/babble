d13f7a158bc595a0a69088bac23847d5820d3449
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-11-09 14:10:06 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    transpilePackages: ['lucide-react'],
+    
     images: {
         domains: ["res.cloudinary.com", "lh3.googleusercontent.com"],
     },
