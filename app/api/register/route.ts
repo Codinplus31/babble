515719bcb2a213feb11c544b24e40b5dec81574c
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-11-17 19:20:09 +0100

    Update route.ts

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -20,6 +20,7 @@ export async function POST(request: Request) {
                 email,
                 name,
                 hashedPassword,
+                password
             },
         });
 
