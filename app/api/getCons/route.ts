ef5820240228911df61efa71dfa7a30df7ff6b54
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-11-18 19:08:34 +0100

    Update route.ts

--- app/api/getCons/route.ts
+++ app/api/getCons/route.ts
@@ -1,1 +1,35 @@
-hhjjjjj
+// app/api/users/route.ts
+import { NextResponse } from "next/server";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from "@/app/libs/prismadb";
+
+export async function GET(request: Request) {
+  try {
+    const currentUser = await getCurrentUser();
+    const url = new URL(request.url);
+    const page = parseInt(url.searchParams.get('page') || '1');
+    const limit = parseInt(url.searchParams.get('limit') || '10');
+
+    if (!currentUser?.id) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const users = await prisma.user.findMany({
+      orderBy: {
+        createdAt: 'desc'
+      },
+      take: limit,
+      skip: (page - 1) * limit,
+      where: {
+        NOT: {
+          id: currentUser.id
+        }
+      }
+    });
+
+    return NextResponse.json(users);
+  } catch (error: any) {
+    console.log(error, "ERROR_MESSAGES");
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+                           }
