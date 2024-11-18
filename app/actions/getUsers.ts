f3277d03a0477173742c09de72f929e7726070f2
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2024-11-18 20:01:25 +0100

    Update getUsers.ts

--- app/actions/getUsers.ts
+++ app/actions/getUsers.ts
@@ -8,13 +8,17 @@ const getUsers = async () => {
     if (!session?.user?.email) {
         return [];
     }
-
+const skip = (page - 1) * limit;
+    const currentUser = await getCurrentUser();
+    
     try {
         //    Find all other users present
         const users = await prisma.user.findMany({
             orderBy: {
                 createdAt: "desc",
             },
+            take: limit,
+            skip: skip,
             where: {
                 NOT: {
                     email: session.user.email,
