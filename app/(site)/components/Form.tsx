ee5dbfe54dbe6d7891306c3e8ae7d044fce7f19a
Uhunoma Paul <127848046+Codinplus31@users.noreply.github.com>
2025-03-30 03:09:07 +0100

    Update Form.tsx

--- app/(site)/components/Form.tsx
+++ app/(site)/components/Form.tsx
@@ -100,7 +100,8 @@ const User = () => {
             axios
               .get("/api/auth/session")
               .then((response) => {
-                const userId = response.data.user.id
+                const userId = response.data.user.id;
+                console.log(userId)
                 router.push(`/conversations?id=${userId}`)
                 setTimeout(() => window.location.reload(), 4000)
               })
